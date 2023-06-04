00a15391b4e5f1ad491bafc83a118ee22591481b
alekseenkos5660 <alekseenkoredmi4a@gmail.com>
2023-06-04 14:51:06 +0300

    Added solution to the second task

--- hometask2.py
+++ hometask2.py
@@ -2,3 +2,17 @@
 # выводит на экран максимум из трёх, минимум из трёх или среднеарифметическое трёх чисел.
 # 2. Пользователь вводит с клавиатуры количество метров. В зависимости от выбора пользователя программа переводит
 # метры в мили, дюймы или ярды.
+
+# meters = float(input("Enter distance in meters: "))
+# choice_one = "Enter 1 if you want to convert distances to miles."
+# choice_two = "Enter 2 if you want to convert distances to inches."
+# choice_three = "Enter 3 if you want to convert distances to yards."
+# user_choice = int(input(f"\t{choice_one} \n\t{choice_two} \n\t{choice_three} \nMake your choise: "))
+# if user_choice == 1:
+#     print("\tResult:", meters/1609)
+# elif user_choice == 2:
+#     print("\tResult:", meters*39.37)
+# elif user_choice == 3:
+#     print("\tResult:", meters*1.094)
+# else:
+#     print("\tResult: Wrong choice. Make a choice from 1 to 3")
