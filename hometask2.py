ac6c6599b08f36958a4cd8ac753897e929b127c8
alekseenkos5660 <alekseenkoredmi4a@gmail.com>
2023-06-04 14:48:06 +0300

    Added solution for the first task

--- hometask2.py
+++ hometask2.py
@@ -1,4 +1,35 @@
 # 1. Пользователь вводит с клавиатуры три числа. В зависимости от выбора пользователя программа
 # выводит на экран максимум из трёх, минимум из трёх или среднеарифметическое трёх чисел.
+
+# num_one = int(input("Enter first number: "))
+# num_two = int(input("Enter second number: "))
+# num_three = int(input("Enter third number: "))
+# choice_one = "Enter 1 to see the largest of the three numbers."
+# choice_two = "Enter 2 to see the smallest of three numbers."
+# choice_three = "Enter 3 to see the average of three numbers."
+# user_choice = int(input(f"\t{choice_one} \n\t{choice_two} \n\t{choice_three} \nMake your choice: "))
+# if user_choice == 1:
+#     if num_one > num_two and num_one > num_three:
+#         print("\tResult:", num_one)
+#     elif num_two > num_one and num_two > num_three:
+#         print("\tResult:", num_two)
+#     elif num_three > num_one and num_three > num_two:
+#         print("\tResult:", num_three)
+#     else:
+#         print("\tResult: Entered numbers are equal")
+# elif user_choice == 2:
+#     if num_one < num_two and num_one < num_three:
+#         print("\tResult:", num_one)
+#     elif num_two < num_one and num_two < num_three:
+#         print("\tResult:", num_two)
+#     elif num_three < num_one and num_three < num_two:
+#         print("\tResult:", num_three)
+#     else:
+#         print("\tResult: Entered numbers are equal")
+# elif user_choice == 3:
+#     print("\tResult:", (num_one+num_two+num_three)/3)
+# else:
+#     print("\tResult: Wrong choice. Make a choice from 1 to 3")
+
 # 2. Пользователь вводит с клавиатуры количество метров. В зависимости от выбора пользователя программа переводит
 # метры в мили, дюймы или ярды.
